4d85fdea7685f5a01e2656a9fa1103b273bfd483
Han Wang <hyw2@cornell.edu>
2021-10-22 20:25:39 -0400

    Add recursive file content traversal

--- out/extension.js
+++ out/extension.js
@@ -13,14 +13,42 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('mintlify.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand('mintlify.helloWorld', async () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from !');
+        const root = vscode.workspace.workspaceFolders[0].uri;
+        const filesContent = await traverseFiles(root, []);
+        console.log(filesContent);
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+async function traverseFiles(root, filesContent) {
+    const files = await vscode.workspace.fs.readDirectory(root);
+    const filePromises = files.map(async (file, i) => {
+        // If filetype is a file
+        if (file[1] === 1) {
+            const filePath = `${root}/${file[0]}`;
+            const readFileUri = vscode.Uri.parse(filePath);
+            const readFileRaw = await vscode.workspace.fs.readFile(readFileUri);
+            const readFileContent = { path: filePath, content: readFileRaw.toString() };
+            filesContent.push(readFileContent);
+        }
+        else if (file[1] === 2 && isTraversablePath(file[0])) {
+            const newRoot = vscode.Uri.parse(`${root}/${file[0]}`);
+            await traverseFiles(newRoot, filesContent);
+        }
+    });
+    await Promise.all(filePromises);
+    return filesContent;
+}
+function isTraversablePath(folderName) {
+    const nonTraversable = {
+        "node_modules": true,
+        ".git": true,
+    };
+    return !nonTraversable[folderName];
+}
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+type File = {
+	path: string;
+	content: string;
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,13 +18,45 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mintlify.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('mintlify.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
+		const root = vscode.workspace.workspaceFolders![0]!.uri;
+		const filesContent = await traverseFiles(root, []);
+
+		console.log(filesContent);
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+async function traverseFiles(root: vscode.Uri, filesContent: File[]): Promise<File[]> {
+	const files = await vscode.workspace.fs.readDirectory(root);
+	const filePromises = files.map(async (file, i) => {
+		// If filetype is a file
+		if (file[1] === 1) {
+			const filePath = `${root}/${file[0]}`;
+			const readFileUri = vscode.Uri.parse(filePath);
+			const readFileRaw = await vscode.workspace.fs.readFile(readFileUri);
+			const readFileContent = { path: filePath, content: readFileRaw.toString()};
+			filesContent.push(readFileContent);
+		} else if (file[1] === 2 && isTraversablePath(file[0])) {
+			const newRoot = vscode.Uri.parse(`${root}/${file[0]}`);
+			await traverseFiles(newRoot, filesContent);
+		}
+
+	});
+	await Promise.all(filePromises);
+	return filesContent;
+}
+
+function isTraversablePath(folderName: string): boolean {
+	const nonTraversable: Record<string, boolean> = {
+		"node_modules": true,
+		".git": true,
+	};
+
+	return !nonTraversable[folderName];
 }
 
 // this method is called when your extension is deactivated
